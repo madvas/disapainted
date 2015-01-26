47548f949b976beded99a48079808a98351ade85
madvas <matus.lestan@gmail.com>
2015-01-26 19:43:48 +0100

    changed inputs in canvas page from bootstrap to material style

--- public/modules/canvas/canvas/dialogs/publish-canvas.client.controller.js
+++ public/modules/canvas/canvas/dialogs/publish-canvas.client.controller.js
@@ -12,18 +12,10 @@
     /* jshint validthis: true */
     var vm = this;
 
-    vm.charsLeft = charsLeft;
     vm.answer = answer;
     vm.AnimsConfig = AnimsConfig;
 
     ////////////////
-
-
-    function charsLeft(isValid) {
-      if (!isValid) return 0;
-      if (!vm.desc) return AnimsConfig.descMaxLength;
-      return AnimsConfig.descMaxLength - vm.desc.length;
-    }
 
     function answer(ans) {
       $mdDialog.hide(ans);
